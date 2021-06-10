f490a3c06b0e8e1ad06b226b1c7b0a8be968016d
Kara LeSueur <62776843+klesueur@users.noreply.github.com>
2021-06-10 10:39:29 -0700

    arrow functions connected to onClicks

---
+++ src/App.svelte
@@ -0,0 +1,37 @@
+<script>
+	let name = "John Doe";
+	let points = 100;
+
+	const addPoint = () => {
+		points += 1
+	};
+
+	const removePoint = () => {
+		points -= 1
+	};
+</script>
+
+<style>
+	/* main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	} */
+	h1 {
+		color: #ff3e00;
+	}
+</style>
+
+<div class="container">
+	<div class="card">
+		<h1>{name}</h1>
+		<h3>{points}</h3>
+		<button class="btn" on:click={addPoint}>
+			+1
+		</button>
+		<button class="btn btn-dark" on:click={removePoint}>
+			-1
+		</button>
+	</div>
+</div>
