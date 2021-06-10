c284fb083746c02f7862648d5151f34d7c5339ba
Kara LeSueur <62776843+klesueur@users.noreply.github.com>
2021-06-10 11:24:28 -0700

    clean up

--- src/Player.svelte
+++ src/Player.svelte
@@ -1,6 +1,8 @@
 <script>
-    export let name;
-    export let points;
+	import Navbar from "./Navbar.svelte";
+
+	let name = "John Doe";
+	let points = 100;
 	let showControls = false;
 
 	const addPoint = () => {
@@ -16,21 +18,40 @@
 	};
 </script>
 
-<div class="card">
-    <h1>
-        {name}
-        <button class="btn-toggle" on:click={toggleControls}>
-            {#if showControls} - {:else} + {/if}
-        </button>
-    </h1>
-    <h3>Points: {points}</h3>
-    {#if showControls}
-        <button class="btn" on:click={addPoint}>
-            +1
-        </button>
-        <button class="btn btn-dark" on:click={removePoint}>
-            -1
-        </button>
-        <input type="number" bind:value={points} />
-    {/if}
+<style>
+	/* main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	} */
+	h1 {
+		color: #ff3e00;
+	}
+    h3 {
+        margin-bottom: 10px;
+    }
+</style>
+
+<Navbar />
+
+<div class="container">
+	<div class="card">
+		<h1>
+			{name}
+			<button class="btn-toggle" on:click={toggleControls}>
+				{#if showControls} - {:else} + {/if}
+			</button>
+		</h1>
+		<h3>Points: {points}</h3>
+		{#if showControls}
+			<button class="btn" on:click={addPoint}>
+				+1
+			</button>
+			<button class="btn btn-dark" on:click={removePoint}>
+				-1
+			</button>
+			<input type="number" bind:value={points} />
+		{/if}
+	</div>
 </div>
